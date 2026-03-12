548d2e632edeb202de09d7218efa6488477596ac
luis <luisnavarrodelangel@gmail.com>
2026-03-11 20:40:59 -0400

    updates

--- app.js
+++ app.js
@@ -17,43 +17,55 @@ const App = {
 
 
     const modules = [
+      // {
+      //   title: "",
+      //   category: "General Overview",
+      //   url: "",
+      //   image: ""
+
+      // },
       {
         title: "Temporal Tides",
         url: "Nimrat, Omaimah & Kareena/index.html",
+        category: "Human versus Whale Time",
         image: "images/temporalTides.png"
-      }, 
+      },
       {
         title: "Ocean Commotion",
         url: "Erin, Hannah & Madeline/index.html",
+        category: "The Anthropocene and Consumer Culture",
         image: "images/oceanCommotion.png"
-      }, 
-      {
-        title: "Indigenous Perspectives in Reducing Whale Harm in Shipping Practice",
-        url: "Claire & Geneva/index.html", 
-        image: "images/indigenousPerspectives.png"
-      }, 
-      {
-        title: "Dive into the world of whales!",
-        url: "Safiya, Ava & Naila/index.html", 
-        image: "images/diveIntoTheWorlOfWhales.png"
-      }, 
+      },
       {
         title: "Jeopardy: Whale Health",
-        url: "Rowan, Anh, and Jing Yi/ModuleJeopardy.html", 
+        category: "Integrated Cognitive Framework  ",
+        url: "Rowan, Anh, and Jing Yi/ModuleJeopardy.html",
         image: "images/WhaleJeopardy.png"
-      }, 
+      },
+      {
+        title: "Indigenous Perspectives in Reducing Whale Harm in Shipping Practice",
+        url: "Claire & Geneva/index.html",
+        category: "Indigenous Perspectives in Canada",
+        image: "images/indigenousPerspectives.png"
+      },
+      {
+        title: "Dive into the world of whales!",
+        url: "Safiya, Ava & Naila/index.html",
+        category: "Traditional Ecological Knowledge ",
+        image: "images/diveIntoTheWorlOfWhales.png"
+      },
+      
+
       {
         title: "",
-        url: ""
-      }, 
-      {
-        title: "",
-        url: ""
+        category: "Traditional Ecological Knowledge ",
+        url: "", 
+        image: ""
       }
     ]
 
 
-  
+
 
     return {
       openDrawer,
